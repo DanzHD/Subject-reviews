50ad19431dbb1c2da3fde8fa549e8a1bde9a422e
Duongdh <duongdh@student.unimelb.edu.au>
2023-06-30 01:19:23 +1000

    Added new event listeners on new subjects that appear

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("This is a test");
+console.log("hello");
+
+function AddSubjectListeners() {
+  let subjects = document.querySelectorAll(".search-result-item__anchor");
+  subjects.forEach((subject) => {
+
+      if (!subject.getAttribute('listener')) {
+        subject.addEventListener('mouseover', () => console.log("hovering"));
+        subject.setAttribute('listener', true);
+      }
+    }
+  );
+}
+AddSubjectListeners();
+/* Adds listeners on new subject refreshes */
+const wrapper = document.querySelector('.search-results-wrapper');
+const config = {attributes: true, attributeFilter: ['class']};
+const obsever = new MutationObserver( (mutations) => {
+  mutations.forEach( (mutation) => {
+    AddSubjectListeners();
+  })
+});
+obsever.observe(wrapper, config);
+
