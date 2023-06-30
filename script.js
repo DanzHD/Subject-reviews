16e8577cedf88133236b576d4e8f399073fdc6c9
Duongdh <duongdh@student.unimelb.edu.au>
2023-06-30 20:51:14 +1000

    Created getRatings function

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ function AddSubjectListeners() {
   subjects.forEach((subject) => {
 
       if (!subject.getAttribute('listener')) {
-        subject.addEventListener('mouseover', () => console.log("hovering"));
+        subject.addEventListener('mouseover', () => {
+          console.log("testing");
+          
+        });
         subject.setAttribute('listener', true);
       }
     }
@@ -16,9 +19,35 @@ AddSubjectListeners();
 const wrapper = document.querySelector('.search-results-wrapper');
 const config = {attributes: true, attributeFilter: ['class']};
 const obsever = new MutationObserver( (mutations) => {
-  mutations.forEach( (mutation) => {
+  mutations.forEach( () => {
     AddSubjectListeners();
   })
 });
-obsever.observe(wrapper, config);
+if (wrapper) obsever.observe(wrapper, config);
 
+async function getReviewsRating(subjectCode) {
+  
+  console.log("In getReviewsRating");
+  const url = "https://studentvip.com.au/unimelb/subjects/" + subjectCode;
+  let numberOfStars = fetch(url)
+    .then(response => response.text())
+    .then(responseText => {
+
+      let parser = new DOMParser();
+      let reviewDoc = parser.parseFromString(responseText, "text/html");
+      let rating = reviewDoc.querySelector('.rating');
+      let stars = rating.children;
+      let numberOfStars = 0;
+      const MAX_RATING = 5
+      for (let i = 0; i < MAX_RATING; i++) {
+
+        if (stars[i].matches(".fas.fa-star") === true) {
+          numberOfStars++;
+        }
+      }
+      return numberOfStars;
+    });
+    return numberOfStars;
+
+}
+getReviewsRating("COMP20008");
