3f380d9e4dbd2fc32fcbdd2bd1d67bd96d6bd496
Duongdh <duongdh@student.unimelb.edu.au>
2023-07-01 21:57:37 +1000

    Added function to get rating and listener to fetch urls

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    fetch(request.url)
+    .then( (response) => { 
+      let responseText = response.text();
+      console.log(responseText);
+      responseText.then( (docText) => {
+        sendResponse({doc: docText});
+      })
+    });
+      
+    return true;
+  }
+  
+);
--- script.js
+++ script.js
@@ -25,29 +25,30 @@ const obsever = new MutationObserver( (mutations) => {
 });
 if (wrapper) obsever.observe(wrapper, config);
 
-async function getReviewsRating(subjectCode) {
+async function getReviewsRating(docText) {
   
   console.log("In getReviewsRating");
-  const url = "https://studentvip.com.au/unimelb/subjects/" + subjectCode;
-  let numberOfStars = fetch(url)
-    .then(response => response.text())
-    .then(responseText => {
+  let parser = new DOMParser();
+  let reviewDoc = parser.parseFromString(docText, "text/html");
+  let rating = reviewDoc.querySelector('.rating');
+  let stars = rating.children;
+  let numberOfStars = 0;
+  const MAX_RATING = 5
+  for (let i = 0; i < MAX_RATING; i++) {
 
-      let parser = new DOMParser();
-      let reviewDoc = parser.parseFromString(responseText, "text/html");
-      let rating = reviewDoc.querySelector('.rating');
-      let stars = rating.children;
-      let numberOfStars = 0;
-      const MAX_RATING = 5
-      for (let i = 0; i < MAX_RATING; i++) {
+    if (stars[i].matches(".fas.fa-star") === true) {
+      numberOfStars++;
+    }
+  }
 
-        if (stars[i].matches(".fas.fa-star") === true) {
-          numberOfStars++;
-        }
-      }
-      return numberOfStars;
-    });
-    return numberOfStars;
+  return numberOfStars;
 
 }
-getReviewsRating("COMP20008");
+(async () => {
+  const responseText = await chrome.runtime.sendMessage({url: "https://studentvip.com.au/unimelb/subjects/COMP20008"});
+  
+  let rating = getReviewsRating(responseText.doc);
+  console.log(rating);
+  
+})();
+//console.log(getReviewsRating("COMP20008"));
