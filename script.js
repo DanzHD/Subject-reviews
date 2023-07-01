9aaf5dcd49b7f64fcc79e1d7b588ce1f1f3b2f93
Duongdh <duongdh@student.unimelb.edu.au>
2023-07-01 23:30:35 +1000

    Connected the hover event listeners to the getRatings

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 console.log("hello");
+const baseURL = "https://studentvip.com.au/unimelb/subjects/";
 
 function AddSubjectListeners() {
-  let subjects = document.querySelectorAll(".search-result-item__anchor");
-  subjects.forEach((subject) => {
+  let listings = document.querySelectorAll(".search-result-item__anchor");
+  listings.forEach((listing) => {
+    let code = listing.querySelector('.search-result-item__header .search-result-item__code').textContent.toString();
+    if (!isSubject(code)) return;
 
-      if (!subject.getAttribute('listener')) {
-        subject.addEventListener('mouseover', () => {
-          console.log("testing");
-          
+    if (!listing.getAttribute('listener')) {
+      listing.addEventListener('mouseover', () => {
+        let code = listing.querySelector('.search-result-item__header .search-result-item__code').textContent.toString();
+        console.log(code);
+        let subjectURL = baseURL + code;
+        let rating = getRating(subjectURL);
+        rating.then ((a) => {
+          console.log(a);
         });
-        subject.setAttribute('listener', true);
-      }
+
+
+      });
+      listing.setAttribute('listener', true);
     }
-  );
+  });
 }
 AddSubjectListeners();
 /* Adds listeners on new subject refreshes */
@@ -31,11 +40,15 @@ async function getReviewsRating(docText) {
   let parser = new DOMParser();
   let reviewDoc = parser.parseFromString(docText, "text/html");
   let rating = reviewDoc.querySelector('.rating');
+  if (!rating) return null;
+
   let stars = rating.children;
   let numberOfStars = 0;
   const MAX_RATING = 5
+  if (!stars) return null;
+  if (stars[0].matches(".fa.fa-arrow-down.fa-3x")) return null;
+  
   for (let i = 0; i < MAX_RATING; i++) {
-
     if (stars[i].matches(".fas.fa-star") === true) {
       numberOfStars++;
     }
@@ -44,11 +57,32 @@ async function getReviewsRating(docText) {
   return numberOfStars;
 
 }
-(async () => {
-  const responseText = await chrome.runtime.sendMessage({url: "https://studentvip.com.au/unimelb/subjects/COMP20008"});
-  
+
+async function getRating(subjectURL) {
+  const responseText = await chrome.runtime.sendMessage({url: subjectURL});
+
   let rating = getReviewsRating(responseText.doc);
-  console.log(rating);
   
-})();
-//console.log(getReviewsRating("COMP20008"));
+  return rating;
+};
+
+
+function isSubject(code) {
+  console.log("In isSubject");
+  if (code.length != 9) {
+    return false;
+  }
+
+  // Check if not all letters
+  if (/[^a-z]/i.test(code.slice(0, 4))) {
+    return false;
+  }
+
+  // check if number
+  if (isNaN(code.slice(4, 9))) {
+    return false;
+  }
+  return true;
+
+}
+
